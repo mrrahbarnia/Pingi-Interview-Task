973f42531ed62c0fc7c10dfed2ccf1aa4d1dea58
Mohammadreza Rahbarnia <mrrahbarnia@gmail.com>
2026-07-04 18:28:33 +0330

    Added products model

--- src/apps/products/models.py
+++ src/apps/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    capacity = models.PositiveIntegerField(default=1_000_000)
+
+    registration_count = models.PositiveIntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
